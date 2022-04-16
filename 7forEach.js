7e2e668eb85a532c9ac34b7717b3a71ba2af0bfa
Valeria Vitvytska <leranmu@gmail.com>
2022-04-16 17:51:31 +0300

    Update 7forEach.js

--- 7forEach.js
+++ 7forEach.js
@@ -2,43 +2,12 @@
 
 function filterBy(array, datatype) {
 
+        let newArray = [];
 
-	let newArray = [];
+        array.forEach((elem, index) => { if (typeof array[index] !== datatype) { newArray.push(array[index]) } });
 
+        return newArray;
 
-	switch (datatype) {
-
-		case 'number':
-
-			array.forEach((elem, index) => { if (typeof array[index] !== 'number') { newArray.push(array[index]) } });
-
-
-			break;
-
-
-		case 'string':
-
-			array.forEach((elem, index) => { if (typeof array[index] !== 'string') { newArray.push(array[index]) } });
-
-
-			break;
-
-		case 'object':
-
-			array.forEach((elem, index) => { if (typeof array[index] !== 'object') { newArray.push(array[index]) } });
-
-
-			break;
-
-		case 'boolean':
-
-			array.forEach((elem, index) => { if (typeof array[index] !== 'boolean') { newArray.push(array[index]) } });
-
-
-			break;
-	}
-
-	return newArray;
 }
 
 const taskArray = ['hello','world',23,'23',null];
