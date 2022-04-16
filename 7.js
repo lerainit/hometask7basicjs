987ed189b94ef4bfd69632e07b31734bfbd96558
Valeria Vitvytska <leranmu@gmail.com>
2022-04-16 09:35:54 +0300

    7commit

---
+++ 7.js
@@ -0,0 +1,47 @@
+
+
+function filterBy(array, datatype) {
+
+
+	let newArray;
+
+
+	switch (datatype) {
+
+		case 'number':
+
+			newArray = array.filter((elem) => typeof elem !== 'number');
+			
+			break;
+
+        case 'string':
+
+			newArray = array.filter((elem) => typeof elem !== 'string');
+
+			break;
+
+		case 'object':
+
+			newArray = array.filter((elem) => typeof elem !== 'object');
+			
+			break;
+
+		case 'boolean':
+
+			newArray = array.filter((elem) => typeof elem !== 'boolean');
+
+			break;
+	}
+	
+	return newArray;
+}
+
+const taskArray = ['hello','world',23,'23',null];
+
+const myArray = ['lera', 32, 'vika', 34, { name: 'lera', surname: 'vitv' }, { name: 'vika', suraname: 'vitv' }, true, false];
+
+const allTypes = ['number', 'string', 'object', 'boolean'];
+
+allTypes.forEach((type) => console.log(filterBy(myArray, type)));
+
+allTypes.forEach((type) => console.log(filterBy(taskArray,type)));
